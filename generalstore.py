a7ef7f8e945d7a77a3b13b3779773b839136ade7
totallyadanna <adannanjokuuk@gmail.com>
2025-03-23 12:43:13 +0000

    generalstore.py

--- generalstore.py
+++ generalstore.py
@@ -18,11 +18,23 @@ while True :
         price_each = num * price_cloth
 
     elif user_choice == 2:
-        num + int(input("How many grocery items are you buying ?"))
+        num = int(input("How many grocery items are you buying ?"))
         price_grocery = 3.5
-        print ("Cost of  each grocery item = £",price_grocery)
+        print ("Cost of each grocery item = £",price_grocery)
         price_each = num * price_grocery
 
     elif user_choice == 3:
         num = int(input("How many toys items are you buying ? "))
         price_toys = 4
+        print ("Cost of  each toy item = £",price_toys)
+        price_each = num * price_toys
+
+    total = total + price_each
+    print ("Price = {}".format (price_each))
+    print ("-"*40)
+    
+    yes_no = input ("Do you want to continue (y/n) ? ")
+    if yes_no == "n" or yes_no == "No" or yes_no == "no" or yes_no == "NO" :
+        break
+
+print ("Your total bill = £",total)
